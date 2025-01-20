b9ced83e6cdd42595d7ca762fa54e17ef37d755b
zaianb <zainnylolade@gmail.com>
2025-01-20 11:27:01 +0100

    make cart quantity button is interactive

--- scripts/Amazon.js
+++ scripts/Amazon.js
@@ -76,6 +76,13 @@ products.forEach((product) => {
   productId: productId,
   quantity:1
  }) 
-}
+} 
+let cartQuantity = 0;
+cart.forEach((item) => {
+  cartQuantity += item.quantity;
+})
+
+ document.querySelector('.js-cart-quantity')
+ .innerHTML = cartQuantity
 })
  });
