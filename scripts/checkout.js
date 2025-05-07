c28a3347b3b956c2147ed566a1f284d7279f90fa
zaianb <zainnylolade@gmail.com>
2025-05-07 16:00:43 +0100

    added script

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
-
+import { formatCurrency } from "../styles/utils/money.js";
 let cartSummaryHTML = '';
 cart.forEach((cartItem) => {
 
---
+++ styles/utils/money.js
@@ -0,0 +1,3 @@
+ export function formatCurrency(priceCent){
+return(priceCent/100).toFixed(2); 
+}
